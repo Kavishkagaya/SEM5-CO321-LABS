ec096c50f29a83c2707bd0ecdfa01156e97431d8
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-11-15 14:39:48 +0530

    lab3

--- lab3/ex6.c
+++ lab3/ex6.c
@@ -6,15 +6,20 @@ int count=0;
 
 int main(void){
     TCNT0 = 131;
+    TCNT1 = 34286;
 
     DDRB |= (1 << 5) | (1<<3);
 
     TCCR0A = 0x00;
     TCCR0B = 0x04;
 
+    TCCR1A = 0x00;
+    TCCR1B = 0x04;
+
     sei();
 
     TIMSK0 |= (1 << TOIE0);
+    TIMSK1 |= (1 << TOIE1);
 
     while (1)
     {
@@ -22,7 +27,7 @@ int main(void){
 }
 
 ISR (TIMER0_OVF_vect){
-    if(count >= 50){
+    if(count >= 25){
         PORTB ^= (1 << 5);
         count = 0;
     }
@@ -30,4 +35,9 @@ ISR (TIMER0_OVF_vect){
         count++;
     }
     TCNT0 = 131;
+}
+
+ISR (TIMER1_OVF_vect){
+    PORTB ^= (1 << 3);
+    TCNT1 = 34286;
 }
