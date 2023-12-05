dbe8c273f942cd6d44e568bdb1a2411d7195b23c
Kavishkagaya <kavishkarambukwella@gmail.com>
2023-12-05 19:09:32 +0530

    final

--- lab4/ex1.c
+++ lab4/ex1.c
@@ -4,10 +4,10 @@ int main(void)
 {
     DDRD |= (1 << PD6);
 
-    TCCR0A = (1 << WGM01) | (1 << WGM00) | (1 << COM0A1);
+    TCCR0A = (1 << WGM01) | (1 << WGM00) | (1 << COM0A1) | (1 << COM0A0);
     TCCR0B = (1 << CS01) | (1 << CS00);
 
-    OCR0A = 10;
+    OCR0A = 127;
 
     while (1)
     {
