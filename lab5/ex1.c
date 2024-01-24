d6640096918468aaca90c49d1d9412ff77d04266
Pasindu Rangana <115542129+PasinduRR@users.noreply.github.com>
2024-01-24 13:07:28 +0530

    update lab 05

--- lab5/ex1.c
+++ lab5/ex1.c
@@ -9,21 +9,19 @@ int main(void)
 {
     DDRD = 0xFF; // Set all pins on PORTD as output
 
-    ADMUX = (1 << REFS0) | (1 << MUX0) | (1 << ADLAR); // AVCC with external capacitor at AREF pin, ADC1, Left adjust result
+    ADMUX = (1 << REFS0) | (1 << MUX0) | (1 << ADLAR);                 // AVCC with external capacitor at AREF pin, ADC1, Left adjust result
     ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Enable ADC, Enable ADC interrupt, 128 prescaler
-    DIDR0 = (1 << ADC1D); // Disable digital input buffer on ADC1 pin
+    // DIDR0 = (1 << ADC1D); // Disable digital input buffer on ADC1 pin
 
     startADC();
 
     while (1)
     {
-        if(ADCSRA & (1 << ADIF)) // If conversion is complete
+        if (ADCSRA & (1 << ADIF)) // If conversion is complete
         {
             ADCSRA |= (0 << ADIF); // Clear ADIF
-            PORTD = ADCH; // Output result to PORTD
+            PORTD = ADCH;          // Output result to PORTD
             startADC();
         }
     }
-
 }
-
--- lab5/ex2.c
+++ lab5/ex2.c
@@ -1,36 +1,63 @@
 #include <avr/io.h>
+#include <string.h>
+#include <stdio.h>
 
-void setupADC() {
-    ADMUX = (1 << REFS0) | (1 << MUX0) | (1 << ADLAR); // AVCC with external capacitor at AREF pin, ADC1, Left adjust result
+void startADC()
+{
+    ADCSRA |= (1 << ADSC); // Start conversion
+}
+
+void usart_init()
+{
+    UBRR0 = 0x67;
+
+    UCSR0B = (1 << RXEN0) | (1 << TXEN0);
+
+    UCSR0C = (1 << UCSZ01) | (1 << UCSZ00);
+}
+
+void usart_send(unsigned char data)
+{
+    while (!(UCSR0A & (1 << UDRE0)))
+        ;
+
+    UDR0 = data;
+}
+
+int main(void)
+{
+    DDRD = 0xFF; // Set all pins on PORTD as output
+
+    ADMUX = (1 << REFS0) | (1 << MUX0) | (1 << ADLAR);                 // AVCC with external capacitor at AREF pin, ADC1, Left adjust result
     ADCSRA = (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0); // Enable ADC, Enable ADC interrupt, 128 prescaler
-    DIDR0 = (1 << ADC1D); // Disable digital input buffer on ADC1 pin
-}
+    // DIDR0 = (1 << ADC1D); // Disable digital input buffer on ADC1 pin
 
-void startADC() {
-    // Start an ADC conversion
-    ADCSRA |= (1 << ADSC);
-}
+    startADC();
+    usart_init();
 
-void setupPWM() {
-    // Set Fast PWM mode, non-inverting output
-    TCCR0A = (1 << WGM01) | (1 << WGM00) | (1 << COM0A1);
-
-    // Set prescaler to 64 and start PWM
-    TCCR0B = (1 << CS01) | (1 << CS00);
-}
-
-int main(void) {
-    // Set all pins on PORTD as output
-    DDRD = 0xFF;
-
-    setupADC();
-    setupPWM();
-
-    while (1) {
-        if(ADCSRA & (1 << ADIF)) // If conversion is complete
+    while (1)
+    {
+        if (ADCSRA & (1 << ADIF)) // If conversion is complete
         {
             ADCSRA |= (0 << ADIF); // Clear ADIF
-            OCR0A = ADCH; // Output result to PORTD
+
+            char str[10];
+            sprintf(str, "%d\n", ADCH);
+
+            for (int i = 0; i < strlen(str); i++)
+            {
+                usart_send(str[i]);
+            }
+
+            if (ADCH > 200)
+            {
+                PORTD = 0xFF;
+            }
+            else
+            {
+                PORTD = 0x00;
+            }
+
             startADC();
         }
     }
